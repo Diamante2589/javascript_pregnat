34f368a78cb0c2f3bd24661dae4d3909e0089b27
neberpa <nestorebernalpardo@gmail.com>
2023-06-09 10:06:16 -0700

    se modifo exercise

--- main.js
+++ main.js
@@ -2,26 +2,57 @@ const age=prompt("ingresa tu edad");
 const residence=prompt("reside en algun municipio del pais");
 const isAdult=age>=18;
 const isLocatedNorthFrontier=residence.toUpperCase()==="SI";
-const pregnat=prompt("Se encuentra embrazada en estos momentos");
+const pregnat=prompt("S e encuentra en estado de embarazo");
 const isstatePregnat=pregnat.toUpperCase==="SI";
 const time= prompt("cuantas semanas tiene de embarazo");
+const timePregnat=time>=9;
 const bornAge=prompt("cual es su año de nacimiento");
 const actualAge=2023;
 const sumAge=actualAge-bornAge;
 
-
-
-if((isAdult && isLocatedNorthFrontier)||(isstatePregnat && time>=9)||(sumAge>=30)){
-    document.write("Tranquilo te puedes vacunar ");
+if(isAdult && isLocatedNorthFrontier){
+    document.write("Tranquilo te puedes vacunar <br> ");
     console.log("Tranquilo te puedes vacunar");
 }else{
-    document.write("No te puedes vacunar");
+    document.write("No te puedes vacunar<br>");
     console.log("No te puedes vacunar");
 } 
 
+if(isstatePregnat && timePregnat){
+    document.write("Tranquilo te puedes vacunar <br> ");
+    console.log("Tranquilo te puedes vacunar");
+}else{
+    document.write("No te puedes vacunar <br>");
+    console.log("No te puedes vacunar");
+}
+
+if(sumAge>=30){
+    document.write("Tranquilo te puedes vacunar <br> ");
+    console.log("Tranquilo te puedes vacunar");
+}else{
+    document.write("No te puedes vacunar<br>");
+    console.log("No te puedes vacunar");
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.write(`EL EJERCICIO TERMINO`);
     
     
    
 
 
 
+//const pregnat=prompt("Se encuentra embrazada en estos momentos")
