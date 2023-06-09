b51d65096a7dc30fa6599c8d0ae232a5a91d70c8
neberpa <nestorebernalpardo@gmail.com>
2023-06-09 06:51:51 -0700

    exercise pregnat

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ const age=prompt("ingresa tu edad");
 const residence=prompt("reside en algun municipio del pais");
 const isAdult=age>=18;
 const isLocatedNorthFrontier=residence.toUpperCase()==="SI";
+const pregnat=prompt("Se encuentra embrazada en estos momentos");
+const isstatePregnat=pregnat.toUpperCase==="SI";
+const time= prompt("cuantas semanas tiene de embarazo");
+const bornAge=prompt("cual es su año de nacimiento");
+const actualAge=2023;
+const sumAge=actualAge-bornAge;
 
-if(isAdult && isLocatedNorthFrontier){
-    document.write("Tranquilo te puedes vacunar");
+
+
+if((isAdult && isLocatedNorthFrontier)||(isstatePregnat && time>=9)||(sumAge>=30)){
+    document.write("Tranquilo te puedes vacunar ");
     console.log("Tranquilo te puedes vacunar");
-}
-
-else {
+}else{
     document.write("No te puedes vacunar");
     console.log("No te puedes vacunar");
-}
+} 
+
+    
+    
    
+
 
 
